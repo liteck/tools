d6f3b4451f5356c9a2890c2e7c17c68f458457ea
gchk <3173413@qq.com>
2020-11-16 11:43:25 +0800

    ### gchk ### 1. add post json

---
+++ http.go
@@ -0,0 +1,36 @@
+package tools
+
+import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"net/http"
+)
+
+func PostForm() {
+
+}
+
+func PostJson(url string, jsonData []byte) (data []byte, err error) {
+	var req *http.Request
+	if req, err = http.NewRequest("POST", url, bytes.NewBuffer(jsonData)); err != nil {
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	var resp *http.Response
+	if resp, err = client.Do(req); err != nil {
+		return
+	}
+	if resp.Body == nil {
+		err = errors.New("response body is null")
+		return
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	data, err = ioutil.ReadAll(resp.Body)
+	return
+}
