542b81b5c1063fc4abb1ddeb56458684d004d13f
gchk <3173413@qq.com>
2020-11-25 17:05:44 +0800

    增加文件存在性校验

--- file.go
+++ file.go
@@ -41,3 +41,14 @@ func CreateDir(dirs ...string) error {
 	}
 	return nil
 }
+
+/**
+检查文件是否存在
+*/
+func CheckFileIsExist(filename string) bool {
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
+}
